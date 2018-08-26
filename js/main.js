6a4d0c1573a13a1bf3f6d138faa3b826b73f26fc
JP Encarnacion <jppr293@gmail.com>
2018-08-26 01:41:42 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,18 +1,29 @@
 console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
-var cardOne = "queen";
-var cardTwo = "king";
 var cardsInPlay = [];
-cardsInPlay.push('cardOne');
-cardsInPlay.push('cardTwo');
-cardsInPlay;
-console.log("User flipped" + " " + cardOne);
-console.log("User flipped" + " " + cardTwo);
-if (cardsInPlay.length === 2)
-	console.log("Cards Chosen");
-if (cardsInPlay.length === 2 && cardsInPlay[0]===cardsInPlay[1]){
-	alert("You found a match!");
-}else (cardsInPlay.length === 2 && cardsInPlay[0]!==cardsInPlay[1])
-	alert("Sorry,try again");
+var checkForMatch =function(){
+	if (cardsInPlay[0]===cardsInPlay[1]) {
+		console.log("You found a match!");
+    }else {
+    	console.log("Sorry,try again.");
+    }
+};
+var flipCard = function(cardId) {
+	console.log("User flipped" + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+};
+flipCard(0);
+flipCard(2);
+
+
+
+
+
+
+
+
 
 
